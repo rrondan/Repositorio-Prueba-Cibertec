7a51662d73ccdfbb085c87d659a946f93a3c6b04
Michael <rondanrichard@gmail.com>
2019-10-20 13:27:53 -0500

    test

--- app/src/main/java/pe/edu/cibertec/capitulo4/Main2Activity.java
+++ app/src/main/java/pe/edu/cibertec/capitulo4/Main2Activity.java
@@ -9,6 +9,7 @@ public class Main2Activity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main2);
     }
 }
